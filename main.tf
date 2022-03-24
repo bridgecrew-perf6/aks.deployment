78f7f2812970cc8eb734d5dc350e68064adb85a3
Rocio Ruiz <rocioruizruiz10@gmail.com>
2022-03-24 18:09:39 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "=2.99.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "aksrsg"
+    location = "West Europe"
+    storage_account_name = "rsgsta"
+    container_name       = "tfstate"
+    key                  = ".tfstate"
+  }
 }
 
 # Configure the Microsoft Azure Provider
@@ -12,15 +19,10 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rsg" {
-  name     = "aksrsg"
-  location = "West Europe"
-}
-
 resource "azurerm_kubernetes_cluster" "rsg" {
   name                = "myaks"
-  location            = azurerm_resource_group.rsg.location
-  resource_group_name = azurerm_resource_group.rsg.name
+  location            = azurerm.location
+  resource_group_name = azurerm.resource_group_name
   dns_prefix          = "rsgmyaks"
   kubernetes_version  = "1.21.9"
 
@@ -39,25 +41,20 @@ resource "azurerm_kubernetes_cluster" "rsg" {
   }
 }
 
-resource "azurerm_storage_account" "rsg" {
-  name                     = "rsgsta"
-  resource_group_name      = azurerm_resource_group.rsg.name
-  location                 = azurerm_resource_group.rsg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
 resource "azurerm_container_registry" "rsg" {
   name                = "acr11767"
-  resource_group_name = azurerm_resource_group.rsg.name
-  location            = azurerm_resource_group.rsg.location
+  resource_group_name = azurerm.resource_group_name
+  location            = azurerm.location
   sku                 = "Basic"
   admin_enabled       = false
 }
 
-resource "azurerm_storage_container" "example" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.rsg.name
-  container_access_type = "private"
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.rsg.kube_config.0.client_certificate
 }
 
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.rsg.kube_config_raw
+
+  sensitive = true
+}
